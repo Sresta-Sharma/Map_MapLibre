8204cba92645a075109a1dce1d3f93aabefcfb2e
Sresta <sresta078@academiacollege.edu.np>
2026-03-27 17:53:11 +0545

    feat: added baselayer switcher

--- src/components/MapBasic.tsx
+++ src/components/MapBasic.tsx
@@ -6,12 +6,17 @@ const MapBasic = () => {
   const mapContainer = useRef<HTMLDivElement | null>(null);
   const mapRef = useRef<maplibregl.Map | null>(null);
 
+  const mapStyles = {
+    light: "https://tiles.openfreemap.org/styles/liberty",
+    dark: "https://tiles.stadiamaps.com/styles/alidade_smooth_dark.json",
+  };
+  
   useEffect(() => {
     if (mapRef.current) return;
 
     mapRef.current = new maplibregl.Map({
       container: mapContainer.current!,
-      style: "https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json",
+      style: mapStyles.light,
       center: [85.324, 27.7172], // Kathmandu
       zoom: 10,
     });
@@ -19,10 +24,34 @@ const MapBasic = () => {
     // Zoom, Rotation & FullScreen controls
     mapRef.current.addControl(new maplibregl.NavigationControl(), "top-right");
     mapRef.current.addControl(new maplibregl.FullscreenControl(), "top-right");
-
   }, []);
 
-  return <div ref={mapContainer} className="w-full h-screen" />;
+  const changeBasemap = (styleKey: keyof typeof mapStyles) => {
+    mapRef.current?.setStyle(mapStyles[styleKey]);
+  };
+
+  return (
+    <div className="relative h-screen w-screen">
+      
+        {/* Map */}
+        <div ref={mapContainer} className="h-full w-full" />
+
+        {/* Baselayer Switcher */}
+        <div className="absolute top-4 left-4 z-50">
+        <select
+            onChange={(e) => {
+            const selected = e.target.value as "light" | "dark"; 
+            changeBasemap(selected);
+            }}
+            className="bg-white/90 border border-gray-200 text-sm rounded-lg shadow-md px-3 py-2 cursor-pointer"
+            defaultValue="light" 
+        >
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+        </select>
+        </div>
+    </div>
+  );
 };
 
 export default MapBasic;
